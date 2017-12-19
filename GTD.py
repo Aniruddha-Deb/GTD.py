94ff9d5ef930173a2a34885a75d654d85ae48c82
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2017-12-19 10:08:26 +0530

    Implemented task delete functionality

--- GTD.py
+++ GTD.py
@@ -4,14 +4,6 @@ import argparse
 import sqlite3
 from datetime import datetime
 
-class Task(object):
-    """Object representation of a GTD task"""
-
-    def __init__( self, name, date ):
-        self.name = name
-        self.date = date
-        self.finished = 0
-
 class CommandLineAPI(object):
     """Command-line GTD utility"""
 
@@ -21,19 +13,38 @@ class CommandLineAPI(object):
 
     def ls( self ):
         for row in self.cursor.execute( "SELECT * FROM tasks" ):
-            print( row )
+            row_id = format( row[0], "<3" ) + "| "
+            row_task = format( row[1], "<40" ) + "| "
+            row_date = " "
+            if row[2] != None:
+                row_date = format( row[2], "<" ) 
+            print( row_id + row_task + row_date )
         self.connection.close()
 
     def add( self, name, date ):
-
         if( date == None ):
-            self.cursor.execute( "INSERT INTO tasks (name) VALUES (?)", [name] )
+            self.cursor.execute( "INSERT INTO tasks (name, due_date) \
+                                  VALUES (?, ?)", [name, datetime.now().strftime( "%d.%m.%Y" )] )
         else:
             print( datetime.strptime( date, "%d.%m.%Y" ) )
             self.cursor.execute( "INSERT INTO tasks (name, due_date) \
                                   VALUES (?, ?)", [ name, datetime.strptime( date, "%d.%m.%Y" ) ] )
         self.connection.commit()
         self.connection.close()
+
+    def remove( self, id ):
+        self.cursor.execute( "SELECT * FROM tasks WHERE id=?", [id] )
+        print( self.cursor.fetchone() );
+        print( "This task will be deleted." )
+        print( "Are you sure you want to continue? (y/n)" )
+        choice = input();
+        if( choice == 'y' or choice == 'Y' ):
+            self.cursor.execute( "DELETE FROM tasks WHERE id=?", [id] )
+            self.connection.commit()
+            self.connection.close()
+            print( "Succesfully deleted" )
+        else:
+            print( "Delete command aborted." )
 
 
 parser = argparse.ArgumentParser()
@@ -48,7 +59,7 @@ add.add_argument( "-t", "--task", required=True, help="Task Name" );
 add.add_argument( "-d", "--date", help="Due date of task" );
 
 remove = subparsers.add_parser( 'remove', help="Removes a task" );
-remove.add_argument( "-i", "--id", type=int, help="Task ID" );
+remove.add_argument( "-i", "--id", required=True, type=int, help="Task ID" );
 
 args = parser.parse_args()
 print( args )
@@ -58,4 +69,4 @@ if( args.cmd == "add" ):
 elif( args.cmd == "ls" ):
     CommandLineAPI().ls();
 else:
-    print( "WIP" )
+    CommandLineAPI().remove( args.id );
