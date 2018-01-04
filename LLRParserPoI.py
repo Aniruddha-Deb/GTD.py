f1eac7220e0d00c88726ba651dc2b39d0eb411f8
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2018-01-04 15:54:37 +0530

    Cleaned up code and refined task inheritance

--- LLRParserPoI.py
+++ LLRParserPoI.py
@@ -2,30 +2,50 @@
 
 import sys
 
-class TextParameter( object ):
+class Constants:
+    CMD_PARAMS = [ "-t", "-d", "-i" ]
+    COMPARATORS = [ "-lt", "-le", "-gt", "-ge", "-eq", "-ne" ]
+
+class Command( object ):
+
+    def __init__( self, name ):
+        self.name = name
+        self.task_name = None
+        self.task_date = None
+        self.task_index = None
+
+    def __str__( self ):
+        return "{}:\n\tName: {}\n\tDate: {}\n\tID: {}".format( 
+            self.name, self.task_name, self.task_date, self.task_index )
+
+class Parameter( object ):
+    
+    def __init__( self, value ):
+        self.value = value
+
+class TextParameter( Parameter ):
 
     def __init__( self, value, regexp, like ):
-        self.value = value
+        super().__init__( value )
         self.regexp = regexp
         self.like = like
 
     def __str__( self ):
         return "{}( regexp={}, like={} )".format( self.value, self.regexp, self.like )
 
-class NumericParameter( object ):
+class NumericParameter( Parameter ):
 
     def __init__( self, value, comparator ):
-        self.value = value
+        super().__init__( value )
         self.comparator = comparator
 
     def __str__( self ):
         return "{}( comparator={} )".format( self.value, self.comparator )
 
 def parse_text_param( args, i ):
-    cmd_params = [ "-t", "-d", "-i" ]
     text_param = TextParameter( None, False, False )
 
-    while i < len(args) and args[i] not in cmd_params:
+    while i < len(args) and args[i] not in Constants.CMD_PARAMS:
         if args[i] == "-r":
             text_param.regexp = True
         elif args[i] == "-l":
@@ -39,12 +59,10 @@ def parse_text_param( args, i ):
 
 def parse_numeric_param( args, i ):
 
-    cmd_params = [ "-t", "-d", "-i" ]
-    comparators = [ "-lt", "-le", "-gt", "-ge", "-eq", "-ne" ]
     numeric_param = NumericParameter( None, None )
 
-    while i < len(args) and args[i] not in cmd_params:
-        if args[i] in comparators:
+    while i < len(args) and args[i] not in Constants.CMD_PARAMS:
+        if args[i] in Constants.COMPARATORS:
             numeric_param.comparator = args[i]
         else:
             numeric_param.value = args[i];
@@ -55,22 +73,24 @@ def parse_numeric_param( args, i ):
 
 def main( args=sys.argv ):
 
-    params = [ "-t", "-d", "-i" ]
-
-    index = 0
+    cmd = Command( args[1] )
+    index = 2
     while index < len( args ):
         s = args[index]
-        if s in params:
+        if s in Constants.CMD_PARAMS:
             index += 1
             if s == "-t":
                 (task_name, i) = parse_text_param( args, index )
-                print( task_name )
+                cmd.task_name = task_name
             elif s == "-d":
                 (task_date, i) = parse_numeric_param( args, index )
-                print( task_date )
+                cmd.task_date = task_date
             elif s == "-i":
                 (task_index, i) = parse_numeric_param( args, index )
-                print( task_index )
+                cmd.task_index = task_index
             index = i
         index += 1
+
+    print( cmd )
+    
 main()
