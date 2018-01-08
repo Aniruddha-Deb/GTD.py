d05f4b3452039d0efe1870e1ed84d00879c7a8c7
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2018-01-08 16:28:55 +0530

    properly implemented ls and del commands

--- DAO.py
+++ DAO.py
@@ -3,57 +3,68 @@
 from parser import Constants
 
 def execute_add_command( cmd ):
-
     print( "INSERT INTO tasks (name, due_date) VALUES ({}, {})".format( cmd.task_name.value, cmd.task_date.value ) )
 
-op_map = { "-lt": " <",
-           "-le": " <=",
-           "-gt": " >",
-           "-ge": " >=",
-           "-eq": " =",
-           "-ne": " !=" }
+op_map = { "-lt": "<",
+           "-le": "<=",
+           "-gt": ">",
+           "-ge": ">=",
+           "-eq": "=",
+           "-ne": "!=" }
 
+def add_parameters_to_statement( cmd, stmt ):
+    if cmd.task_name != None or cmd.task_date != None or cmd.task_index != None:
+        stmt.append( "WHERE" )
+        add_task_parameter_to_statement( cmd, stmt )
+        add_date_parameter_to_statement( cmd, stmt )
+        add_id_parameter_to_statement( cmd, stmt ) 
+    return stmt
+
+def add_task_parameter_to_statement( cmd, stmt ):
+    if cmd.task_name != None:
+        if "date" in stmt or "id" in stmt:
+            stmt.append( "AND" )
+        stmt.append( "task" )
+        if cmd.task_name.regexp == True:
+            stmt.append( "REGEXP" )
+        elif cmd.task_name.like == True:
+            stmt.append( "LIKE" )
+        else:
+            stmt.append( "=" )
+        stmt.append( "?" )
+
+def add_date_parameter_to_statement( cmd, stmt ):
+    if cmd.task_date != None :
+        if "task" in stmt or "id" in stmt:
+            stmt.append( "AND" )
+        stmt.append( "date" )
+        add_numeric_parameter_to_statement( cmd, stmt )
+
+def add_id_parameter_to_statement( cmd, stmt ):
+    if cmd.task_index != None:
+        if "task" in stmt or "date" in stmt:
+            stmt.append( "AND" )
+        stmt.append( "id" )
+        add_numeric_parameter_to_statement( cmd, stmt )
+
+def add_numeric_parameter_to_statement( cmd, stmt ):
+    if cmd.task_index.comparator != None:
+        stmt.append( op_map[cmd.task_index.comparator] )
+    else:
+        stmt.append( "=" )
+    stmt.append( "?" )
+  
 def execute_ls_command( cmd ):
 
-    # TODO: Clean up this terrible piece of crap.
-    stmt = "SELECT * FROM tasks"
-    if cmd.task_name != None or cmd.task_date != None or cmd.task_index != None:
-        stmt += " WHERE"
-        added_task = False
-        if cmd.task_name != None:
-            stmt += " task"
-            added_task = True
-            if cmd.task_name.regexp == True:
-                stmt += " REGEXP"
-            elif cmd.task_name.like == True:
-                stmt += " LIKE"
-            else:
-                stmt += " ="
-            stmt += " ?"
-        if cmd.task_date != None :
-            if added_task:
-                stmt += " AND"
-            else:
-                added_task = True
-            stmt += " date"
-            if cmd.task_date.comparator != None:
-                stmt += op_map[cmd.task_date.comparator]
-            else:
-                stmt += " ="
-            stmt += " ?"
-        if cmd.task_index != None:
-            if added_task:
-                stmt += " AND"
-            stmt += " id"
-            if cmd.task_index.comparator != None:
-                stmt += op_map[cmd.task_index.comparator]
-            else:
-                stmt += " ="
-            stmt += " ?"
-    print( stmt )
+    stmt = [ "SELECT", "*", "FROM", "tasks" ]
+    add_parameters_to_statement( cmd, stmt )
+    print( " ".join( stmt ) )
 
 def execute_del_command( cmd ):
-    pass
+    
+    stmt = [ "DELETE", "FROM", "tasks" ]
+    add_parameters_to_statement( cmd, stmt )
+    print( " ".join( stmt ) )
 
 def execute_upd_command( cmd ):
     pass
